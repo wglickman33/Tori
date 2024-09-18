b8b2e8d5cb07b8438f26189131ba92914f66b305
Will Glickman <willglickman@gmail.com>
2024-09-18 17:06:48 -0400

    finish login page with attached styling - need to come back and attach to backend for real user login feel

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,56 +1,85 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import loginIcon from "../../assets/icons/login.svg";
+import emailIcon from "../../assets/icons/mail.svg";
+import lockIcon from "../../assets/icons/lock.svg";
+import unlockIcon from "../../assets/icons/unlock.svg";
 import "./LoginForm.scss";
 
 const LoginForm = () => {
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setIsPasswordVisible((prevState) => !prevState);
+  };
+
   return (
     <section className="login">
-      <div className="login__header-container">
-        {/* This LOGIN h1 is going to be on a separate page */}
-        <h1 className="login__header">LOGIN</h1>
-      </div>
-      <div className="login__form-container">
-        <form className="login__form" id="loginForm">
-          <label className="login__form-label" htmlFor="email">
-            Enter Email
-            <input
-              className="login__form-input"
-              type="email"
-              id="email"
-              name="email"
-            />
-          </label>
-          <label className="login__form-label" htmlFor="password">
-            Enter Password
-            <input
-              className="login__form-input"
-              type="password"
-              id="password"
-              name="password"
-            />
-          </label>
-          <div className="login__forgot-container">
-            <label className="login__save-login" htmlFor="saveLogin">
+      <div className="login__content">
+        <div className="login__header-container">
+          <h1 className="login__header">LOGIN</h1>
+        </div>
+        <div className="login__form-container">
+          <form className="login__form" id="loginForm">
+            <label className="login__form-label" htmlFor="email">
+              Enter Email
               <input
-                className="login__save-login-checkbox"
-                type="checkbox"
-                id="saveLogin"
-                name="saveLogin"
+                className="login__form-input"
+                type="email"
+                id="email"
+                name="email"
+                placeholder="example@example.com"
               />
-              Save Login Information
+              <img
+                className="login__mail-icon"
+                src={emailIcon}
+                alt="email icon"
+              />
             </label>
-            <Link className="login__forgot-link">Forgot Password?</Link>
-          </div>
-          <div className="login__button-container">
-            <button type="submit" className="login__button">
-              <img className="login__icon" alt="login icon" />
-              LOGIN
-            </button>
-          </div>
-          <p className="login__create-account">
-            Don't have an account?{" "}
-            <Link className="login__create-account-link">Create Account</Link>
-          </p>
-        </form>
+            <label className="login__form-label" htmlFor="password">
+              Enter Password
+              <input
+                className="login__form-input"
+                type={isPasswordVisible ? "text" : "password"}
+                id="password"
+                name="password"
+                placeholder="Password"
+              />
+              <img
+                className="login__lock-icon"
+                src={isPasswordVisible ? unlockIcon : lockIcon}
+                alt={isPasswordVisible ? "unlock icon" : "lock icon"}
+                onClick={togglePasswordVisibility}
+              />
+            </label>
+            <div className="login__forgot-container">
+              <label className="login__save-login" htmlFor="saveLogin">
+                <input
+                  className="login__save-login-checkbox"
+                  type="checkbox"
+                  id="saveLogin"
+                  name="saveLogin"
+                />{" "}
+                Save Login Information{" "}
+              </label>
+              <Link className="login__forgot-link">Forgot Password?</Link>
+            </div>
+            <div className="login__button-container">
+              <button type="submit" className="login__button">
+                <img
+                  className="login__login-icon"
+                  src={loginIcon}
+                  alt="login icon"
+                />
+                LOGIN
+              </button>
+            </div>
+            <p className="login__create-account">
+              Don't have an account?{" "}
+              <Link className="login__create-account-link">Create Account</Link>
+            </p>
+          </form>
+        </div>
       </div>
     </section>
   );
